e4f75b202628f40cb2aa6e9f5a814f099478151b
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-03-04 12:46:31 +0600

    using react fragment empty element

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Components/Header/Header';
 import TopHeadline from './Components/TopHeadline/TopHeadline';
+import UseFragment from './Components/UseFragment/UseFragment';
 
 function App() {
     return (
         <div>
             <h2>React Bootstrap</h2>
             <Header></Header>
+            <UseFragment></UseFragment>
             <TopHeadline></TopHeadline>
         </div>
     );
---
+++ src/Components/UseFragment/UseFragment.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+const UseFragment = () => {
+    return (
+        <Fragment>
+          <h3>There is Something</h3>
+          <button>Click Here</button>  
+        </Fragment>
+    );
+};
+
+export default UseFragment;
